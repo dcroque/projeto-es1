7e0751e11ae52fb653a30172f515e6ac7c392cb0
dcroque <matheusdcroque@gmail.com>
2021-03-16 09:27:40 -0300

    Add main structure for Menu class

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 from menu import *
 
 menu = Menu()
-while (True):
-
-    print("O que deseja procurar? ")
-    print("Para sair do programa digite -1")
-    tag = input()
-    if (tag == '-1'):
-        break
-    menu.show_menu(tag)
+menu.main_menu()
--- menu.py
+++ menu.py
@@ -1,7 +1,56 @@
 from terabyte_scrapper import *
+
 class Menu:
     def __init__(self):
-         pass
+         self.options = [
+             ["Nova compra", self.opt_new_cart],
+             ["Carregar compra", self.opt_load_cart],
+             ["Procurar", self.opt_search],
+             ["Exit", self.opt_exit]
+         ]
+
+    def main_menu(self):
+        while True:
+            for i in range(len(self.options)):
+                print(f"{i+1}: {self.options[i][0]}")
+            choice = input("\nChoose an option: ")
+            try:
+                choice = int(choice)
+                choice -= 1
+                is_numerical = True
+            except:
+                is_numerical = False
+
+            function_loaded = False
+            if is_numerical:
+                if choice >= 0 and choice < len(self.options):
+                    selected_opt = self.options[choice][1]
+                    function_loaded = True
+                else:
+                    print(f"Invalid option: Expected number choice between 1 and {len(self.options)}\n")
+            else:
+                for opt in self.options:
+                    if choice == opt[0]:
+                        selected_opt = opt[1]
+                        function_loaded = True
+                        break
+                if not function_loaded:
+                    print(f"Invalid option: Couldn't find an option named {choice}\n")
+            
+            if function_loaded:
+                selected_opt()
+
+    def opt_exit(self):
+        exit()
+
+    def opt_search(self):
+        print(f"Opcao {3} nao implementado ainda")
+
+    def opt_new_cart(self):
+        print(f"Opcao {1} nao implementado ainda")
+
+    def opt_load_cart(self):
+        print(f"Opcao {2} nao implementado ainda")
 
     def show_menu(self, tag):
 
